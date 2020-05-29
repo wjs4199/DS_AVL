efc971571c3bf7eb48cae75c4f9c76cb4e5c6bd1
wjs4199 <63443458+wjs4199@users.noreply.github.com>
2020-05-29 23:44:02 +0900

    Delete manager.h

--- manager.h
+++
@@ -1,8 +0,0 @@
-#include "product.h"
-
-int selectMenu();
-void listProduct(Product *p,int count);
-int selectDataNo(Product *p, int count);
-
-void saveData(Product *p, int count);
-int loadData(Product *p);
